58fdef19d178e93b40426139a353f6928aeab82d
Jon Medhurst <jon.medhurst@linaro.org>
2011-12-02 13:59:18 +0000

    Increase the maximum number of disk partitions to 8
    Linaro's tools create images with more than 4 partitions, so this change
    is needed so vold sees these extra partitions, like the sdcard
    partition.
    
    MAX_PARTS is also deleted because it's not used and to avoid ambiguity.
    
    Change-Id: Ia67f672d3b343b55b8354a09a1d17dd00679cd6f

--- DirectVolume.h
+++ DirectVolume.h
@@ -21,13 +21,11 @@
 
 #include "Volume.h"
 
-#define MAX_PARTS 4
-
 typedef android::List<char *> PathCollection;
 
 class DirectVolume : public Volume {
 public:
-    static const int MAX_PARTITIONS = 4;
+    static const int MAX_PARTITIONS = 8;
 protected:
     PathCollection *mPaths;
     int            mDiskMajor;
